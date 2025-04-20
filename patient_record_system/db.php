70a1be5714bbcdb37dfecd70271824a27e7bbd0b
laibashoukatofficial <laibashoukatofficial@gmail.com>
2025-04-20 12:47:32 +0500

    Update db.php

--- patient_record_system/db.php
+++ patient_record_system/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "db4free.net";
-$username = "laibadev";         // your username
-$password = "Umar@447#";     // your password
-$database = "patient_db";       // your DB name
+$host = " sql12.freesqldatabase.com"; // Replace with your actual host if different
+$username = "sql12774261";           // Your Freesqldatabase username
+$password = "RqXnrjwjCR";          // Your Freesqldatabase password
+$database = "sql12774261";           // Your database name
 
 $connection = new mysqli($host, $username, $password, $database);
 
@@ -10,7 +10,8 @@ if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
-    // Uncomment below line to check connection
+        // Uncomment below line to check connection
 // echo "Database connected successfully!";
     
 ?>
+
