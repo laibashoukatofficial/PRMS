182e6e6a976d09cbab34c3cd1daa1e88fded1d50
laibashoukatofficial <laibashoukatofficial@gmail.com>
2025-04-20 13:21:23 +0500

    Update db.php

--- patient_record_system/db.php
+++ patient_record_system/db.php
@@ -8,10 +8,4 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-
-        // Uncomment below line to check connection
-// echo "Database connected successfully!";
-    
-?>
-
+}  
